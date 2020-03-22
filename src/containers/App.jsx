f656e2e932801ae481c91d02e5acd4f3a5b7ff7c
Willans Misael <willansmisaelromerocondori@gmail.com>
2020-03-22 19:20:33 -0400

    Se agrego el manejo de React Hooks, useState y useEffect dentro de App.js

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -8,38 +9,49 @@ import Footer from '../components/Footer';
 
 import '../assets/styles/App.scss';
 
-const App = () => (
-    <div className="App">
-        <Header />
-        <Search />  
 
-        <Categories title="Mi Lista">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+const App = () => {
+    const [ videos, setVideos ] = useState([]);
 
-        <Categories title="Tendencias"> 
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
+    useEffect(() => {
+        fetch('http://localhost:3000/initialState') //insertamos la direccion de la api
+        .then(response => response.json()) // vemos la respuesta y transformamos en un json
+        .then(data => setVideos(data)); // enviamos el json a la variable setVideos de useState
+    }, []); // agregamos un [] array vacio para evitar el evenloop
+    console.log(videos);
+    return(
+        <div className="App">
+            <Header />
+            <Search />  
 
-            </Carousel>
-        </Categories>
+            <Categories title="Mi Lista">
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
 
-        <Categories title="Originales de Platzi Video">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
+            <Categories title="Tendencias"> 
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
 
-            </Carousel>
-        </Categories>
-        <Footer/>
-    </div>
-);
+                </Carousel>
+            </Categories>
+
+            <Categories title="Originales de Platzi Video">
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+
+                </Carousel>
+            </Categories>
+            <Footer/>
+        </div>
+        );
+    }
 
 export default App;
